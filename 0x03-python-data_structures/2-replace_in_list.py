61b1b7cd3789ab71b5f5da348c3bdc699d9fc148
MostafaRaafat91 <eng2mega@gmail.com>
2023-12-02 14:01:14 -0800

    push fix

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
+    # Check if the index is within the range of the list
     if (0 <= idx < len(my_list)):
+        # Replace the element at the specified index with the new element
         my_list[idx] = element
+        # Return the modified list
+        return my_list
+    else:
+        # If the index is out of range, return the original list
         return my_list
