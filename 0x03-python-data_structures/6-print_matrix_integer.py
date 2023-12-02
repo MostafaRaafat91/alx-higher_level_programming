fb8edc6c7b697bf1300b53a45d94121d22c245e8
MostafaRaafat91 <eng2mega@gmail.com>
2023-12-02 14:03:48 -0800

    push fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    # Iterate over each row of the matrix
     for line in matrix:
+        # Iterate over each element in the row
         for col in range(len(line)):
+            # Print the element with a space separator
             print(
                     "{:d}".format(line[col]),
                     end="" if col == len(line) - 1 else " ")
-            print("")
+        # Print a newline character after each row
+        print("")
