ef0c7e2894dd268fbb08baaaea7c3f954bf5bc80
Mostafa Raafat <eng2mega@gmail.com>
2023-12-04 21:11:55 +0000

    push fix

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,24 +2,21 @@
 #include <Python.h>
 
 /**
- * print_python_list_info - prints python list info
- *
- * @p: PyObject
- * Return: no return
+ * print_python_list_info - prints info about Python lists
+ * @p: Python Object
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 void print_python_list_info(PyObject *p)
 {
-	long int size, i;
-	PyListObject *list;
+	long int list_size, alloc_list, i;
 	PyObject *item;
 
-	size = Py_SIZE(p);
-	printf("[*] Size of the Python List = %ld\n", size);
+	list_size = PyList_Size(p);
+	alloc_list = ((PyListObject *)p)->allocated;
 
-	list = (PyListObject *)p;
-	printf("[*] Allocated = %ld\n", list->allocated);
-
-	for (i = 0; i < size; i++)
+	printf("[*] Size of the Python List = %ld\n", list_size);
+	printf("[*] Allocated = %ld\n", alloc_list);
+	for (i = 0; i < list_size; i++)
 	{
 		item = PyList_GetItem(p, i);
 		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
