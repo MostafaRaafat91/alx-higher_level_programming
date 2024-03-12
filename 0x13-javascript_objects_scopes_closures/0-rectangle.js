87b415ff462001a63e7e325e268e58aebe24008b
MostafaRaafat91 <eng2mega@gmail.com>
2024-03-12 13:10:01 -0700

    fix issue 0

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,15 +1,5 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
-myObject.incr = function () {
-  this.value++;
-};
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
+class Rectangle {
+}
+
+module.exports = Rectangle;
